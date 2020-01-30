ce646f648131dc6215fb7a1ea219cbba7f0f8c67
Aki Sasaki <aki@mozilla.com>
2020-01-30 08:52:22 +0100

    rename `standard` to `privileged`. (#11)
    * rename `standard` to `privileged`.
    
    fixes #10
    
    * update branch protection docs

--- taskcluster/xpi_taskgraph/xpi_manifest.py
+++ taskcluster/xpi_taskgraph/xpi_manifest.py
@@ -36,7 +36,7 @@ base_schema = Schema(
                 Optional("active"): bool,
                 Optional("private-repo"): bool,
                 Required("artifacts"): [basestring],
-                Required("addon-type"): Any("system", "standard"),
+                Required("addon-type"): Any("system", "privileged"),
                 Optional("install-type"): Any("npm", "yarn"),
             }
         ]
