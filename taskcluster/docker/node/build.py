713230c3dde718d91032d76574b96587b4144700
Aki Sasaki <aki@mozilla.com>
2021-10-11 12:42:16 -0600

    ensure uniqueness of addon+version combination by adding buildid
    The addons team wants to ensure uniqueness of each addon+version
    combination; the releng team wants to avoid churn where the development
    teams need to bump versions to test a new release-signed addon.
    
    We're following the langpack model by adding the buildid to the version
    string, which should hopefully address both teams' concerns.

--- taskcluster/docker/node/build.py
+++ taskcluster/docker/node/build.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+from datetime import datetime
 import functools
 import glob
 import hashlib
@@ -63,6 +64,54 @@ def get_package_info():
     return contents
 
 
+def write_package_info(package_info):
+    with open("package.json", "w") as fh:
+        json.dump(package_info, fh)
+    assert get_package_info() == package_info
+
+
+def get_buildid():
+    now = datetime.utcnow()
+    return now.strftime("%Y%m%d%H%M%S")
+
+
+def get_buildid_version(version):
+    """Version schema check+append a `buildid{buildid}` to ensure unique version."""
+    # XXX is there a more precise schema we should verify ourselves against?
+    if len(version.split(".")) not in (1, 2, 3):
+        raise Exception("{version} has too many version parts!")
+    if "buildid" in version:
+        raise Exception("{version} already has a buildid specified!")
+    buildid_version = f"{version}buildid{get_buildid()}"
+    print(f"Buildid version is {buildid_version}")
+    return buildid_version
+
+
+def find_manifests():
+    manifest_list = []
+    for dir_name, subdir_list, file_list in os.walk("."):
+        for dir_ in subdir_list:
+            if dir_ in (".git", "node_modules"):
+                subdir_list.remove(dir_)
+                continue
+        if "manifest.json" in file_list:
+            manifest_list.append(f"{dir_name}/manifest.json")
+    return manifest_list
+
+
+def find_update_manifest_json(buildid_version):
+    """We also have manifest.json files; let's update them as well."""
+    for manifest in find_manifests():
+        print(f"Updating {manifest} version...")
+        with open(manifest) as fh:
+            contents = json.load(fh)
+        old_version = contents["version"]
+        contents["version"] = buildid_version
+        with open(manifest, "w") as fh:
+            json.dump(contents, fh)
+        print(f"    was {old_version}, now {buildid_version}")
+
+
 def cd(path):
     print(f"Changing directory to {path} ...")
     os.chdir(path)
@@ -81,7 +130,7 @@ def get_hash(path, hash_alg="sha256"):
     return h.hexdigest()
 
 
-def check_manifest(path):
+def check_manifest(path, buildid_version):
     xpi = ZipFile(path, "r")
     manifest = {}
     _found = False
@@ -101,6 +150,8 @@ def check_manifest(path):
                 raise Exception(f"{_key}.gecko.id {_id} must end with one of the following suffixes!\n{ID_ALLOWLIST}")
             else:
                 print(f"Add-on id {_id} matches the allowlist.")
+        if manifest["version"] != buildid_version:
+            raise Exception(f"{manifest['version']} doesn't match buildid version {buildid_version}!")
     if not _found:
         raise Exception("Can't find addon ID in manifest.json!")
 
@@ -124,6 +175,14 @@ def main():
     package_info = get_package_info()
 
     revision = get_output(["git", "rev-parse", "HEAD"])
+    orig_version = package_info["version"]
+    buildid_version = get_buildid_version(orig_version)
+
+    # Make sure we use the buildid_version during the build
+    package_info["version"] = buildid_version
+    print(f"Updating package.json version from {orig_version} to {buildid_version}...")
+    write_package_info(package_info)
+    find_update_manifest_json(buildid_version)
 
     build_manifest = {
         "name": xpi_name,
@@ -131,7 +190,7 @@ def main():
         "repo": os.environ[head_repo_env_var],
         "revision": str(revision.rstrip()),
         "directory": os.path.relpath(base_src_dir, os.getcwd()),
-        "version": package_info["version"],
+        "version": buildid_version,
         "artifacts": [],
     }
 
@@ -164,7 +223,7 @@ def main():
         }
         build_manifest["artifacts"].append(artifact_info)
         shutil.copyfile(artifact, target_path)
-        check_manifest(target_path)
+        check_manifest(target_path, buildid_version)
 
     with open(os.path.join(artifact_dir, "manifest.json"), "w") as fh:
         fh.write(json.dumps(build_manifest, indent=2, sort_keys=True))
