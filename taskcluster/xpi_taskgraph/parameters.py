b02c99ea05624939c144ec6d6e39fe7ff6866675
Andrew Halberstadt <ahal@mozilla.com>
2025-04-25 11:41:43 -0400

    ci: make it easier to set the 'xpi_name' parameter
    This is just to make it easier to only build a specific xpi during
    testing. Otherwise every xpi gets built on every force push to the PR.

--- taskcluster/xpi_taskgraph/parameters.py
+++ taskcluster/xpi_taskgraph/parameters.py
@@ -16,3 +16,8 @@ xpi_schema = {
 }
 
 extend_parameters_schema(xpi_schema)
+
+
+def decision_parameters(graph_config, parameters):
+    # Change this to only build a specific xpi during testing.
+    parameters["xpi_name"] = None
