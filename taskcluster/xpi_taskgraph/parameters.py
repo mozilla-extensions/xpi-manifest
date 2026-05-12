20fa3ac61b79eced7bda852e558cd1792da1e8a0
Johan Lorenzo <jlorenzo@mozilla.com>
2026-05-12 12:01:15 +0200

    taskcluster: upgrade to taskgraph v23 (bug 2025695)

--- taskcluster/xpi_taskgraph/parameters.py
+++ taskcluster/xpi_taskgraph/parameters.py
@@ -3,21 +3,34 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 
+from typing import Literal, Optional
+
+from mozilla_version.version import BaseVersion
 from taskgraph.parameters import extend_parameters_schema
-from voluptuous import Any, Optional, Required
+from taskgraph.util.schema import Schema
 
 
 # Please keep this list sorted
-xpi_schema = {
-    Optional("additional_shipit_emails"): Any([str], None),
-    Optional("shipping_phase"): Any("build", "promote", "ship", None),
-    Optional("xpi_name"): Any(str, None),
-    Optional("xpi_revision"): Any(str, None),
-}
+class XpiParameters(Schema, rename=None, forbid_unknown_fields=False, kw_only=True):
+    additional_shipit_emails: Optional[list[str]] = None
+    app_version: Optional[str] = None
+    next_version: Optional[str] = None
+    shipping_phase: Optional[Literal["build", "promote", "ship"]] = None
+    xpi_name: Optional[str] = None
+    xpi_revision: Optional[str] = None
 
-extend_parameters_schema(xpi_schema)
+
+extend_parameters_schema(XpiParameters)
 
 
 def decision_parameters(graph_config, parameters):
     # Change this to only build a specific xpi during testing.
     parameters["xpi_name"] = None
+    if version := parameters.get("version"):
+        parameters["app_version"] = version
+        parameters["next_version"] = str(
+            BaseVersion.parse(version).bump("minor_number")
+        )
+    else:
+        parameters["app_version"] = None
+        parameters["next_version"] = None
